d42d0f9cbcc97a74d58effde6d74606f67c43509
danoscarmike <omeara.dan@gmail.com>
2018-02-16 00:31:37 -0800

    fix bug

--- dependency_sleuth.py
+++ dependency_sleuth.py
@@ -31,13 +31,39 @@ def main(user, destination):
                     else:
                         dep_name = dep
                         dep_range = None
-                    if dep_name not in gcp_python[package][version]:
+                    if dep_name not in gcp_python[package][version]['deps']:
                         gcp_python[package][version]['deps'][dep_name] =\
                                                                     dep_range
     status = print_to_file(gcp_python, destination)
     if status:
         print("Dependencies for all versions of {}'s packages written to file."
               .format(user))
+
+
+def print_to_file(data, destination):
+    ''' Takes a set of package dependency data and a local file destination and
+        writes to file all the dependencies and their versions or version
+        ranges for each PyPI package owned by the passed user. '''
+    fieldnames = ['package_name', 'package_version', 'latest', 'dep_name',
+                  'dep_version']
+    with open('%s/gcp_python.csv' % destination, 'w') as csvfile:
+        print("Printing to file: {}/gcp_python.csv".format(destination))
+        datawriter = csv.DictWriter(csvfile, delimiter=',',
+                                    fieldnames=fieldnames, quotechar='"')
+        datawriter.writeheader()
+        for package in data:
+            for package_version in data[package]:
+                row = {'package_name': package,
+                       'package_version': package_version,
+                       'latest': data[package][package_version]['latest']}
+                if len(data[package][package_version]['deps']) == 0:
+                    datawriter.writerow(row)
+                else:
+                    for dep in data[package][package_version]['deps']:
+                        row.update({'dep_name': dep, 'dep_version':
+                                    data[package][package_version]['deps'][dep]})
+                        datawriter.writerow(row)
+    return True
 
 
 def get_user_packages(user):
@@ -76,32 +102,9 @@ def get_version_dependencies(package, version):
     response = urllib.request.urlopen(version_URL)
     version_json = json.load(response)
     if 'requires_dist' not in version_json["info"]:
-        return
+        return None
     else:
         return version_json["info"]["requires_dist"]
-
-
-def print_to_file(data, destination):
-    ''' Takes a set of package dependency data and a local file destination and
-        writes to file all the dependencies and their versions or version
-        ranges for each PyPI package owned by the passed user. '''
-    fieldnames = ['package_name', 'package_version', 'latest', 'dep_name',
-                  'dep_version']
-    with open('%s/gcp_python.csv' % destination, 'w') as csvfile:
-        print("Printing to file: {}/gcp_python.csv".format(destination))
-        datawriter = csv.DictWriter(csvfile, delimiter=',',
-                                    fieldnames=fieldnames, quotechar='"')
-        datawriter.writeheader()
-        for package in data:
-            row = {'package_name': package}
-            for package_version in data[package]:
-                row.update({'package_version': package_version, 'latest':
-                            data[package][package_version]['latest']})
-                for dep in data[package][package_version]['deps']:
-                    row.update({'dep_name': dep, 'dep_version':
-                                data[package][package_version]['deps'][dep]})
-                    datawriter.writerow(row)
-    return True
 
 
 if __name__ == "__main__":

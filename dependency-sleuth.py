4162a21b690e3d44d2e18c984cab52391a4dbc58
danoscarmike <omeara.dan@gmail.com>
2018-02-15 17:01:59 -0800

    refactor functions

--- dependency-sleuth.py
+++ dependency-sleuth.py
@@ -14,43 +14,45 @@ def get_package_list_by_user(user):
     print "Calling {}".format(pypi_user_URL)
     soup = BeautifulSoup(urllib2.urlopen(pypi_user_URL), 'html.parser')
     package_list = []
+    dependencies = {}
     for heading in soup.find_all("h3", {"class": "package-snippet__title"}):
         package_list.append(heading.get_text())
-    return package_list
+    for package in package_list:
+        required = get_package_dependencies(package)
+        if not required:
+            pass
+        else:
+            for dist in required:
+                dist_name = dist.split(" ",1)[0]
+                if dist.find(" ") > -1:
+                    dist_range = dist.split(" ",1)[1].split(";",1)[0]
+                else:
+                    dist_range = None
+                if dist_name not in dependencies:
+                    dependencies[dist_name] = {}
+                dependencies[dist_name][package] = dist_range
+    # print json.dumps(dependencies, indent=2)
+    return package_list, dependencies
 
 
 def print_package_list_to_file(user, destination):
-    dependencies = {}
+    package_list, dependencies = get_package_list_by_user(user)
+    print dependencies
     with open('%s/packages.csv' % destination, 'w') as csvfile:
-        package_list = get_package_list_by_user(user)
         fieldnames = ['dependency'] + package_list
         datawriter = csv.DictWriter(csvfile, delimiter=',',
                                     fieldnames=fieldnames, quotechar='"')
         datawriter.writeheader()
-        for package in package_list:
-            required = get_package_deps(package)
-            if not required:
-                pass
-            else:
-                for dist in required:
-                    dist_name = dist.split(" ",1)[0]
-                    if dist.find(" ") > -1:
-                        dist_range = dist.split(" ",1)[1].split(";",1)[0]
-                    else:
-                        dist_range = None
-                    if dist not in dependencies:
-                        dependencies[dist] = {}
-                        dependencies[dist][package] = dist_range
-                    row = {'dependency':dist.split(" ",1)[0]}
-                    if dist.find(" ") > -1:
-                        row.update({package:dist.split(" ",1)[1]})
-                print row
-                datawriter.writerow(row)
+        for dist_name in dependencies:
+            row = {'dependency':dist_name}
+            for package_name in dependencies[dist_name]:
+                row.update({package_name:dependencies[dist_name][package_name]})
+            datawriter.writerow(row)
     print "Dependencies of packages belonging to PyPI user, {},\
            written to file.".format(user)
 
 
-def get_package_deps(package):
+def get_package_dependencies(package):
     package_URL = "http://pypi.python.org/pypi/{}/json".format(package)
     print ("Calling {}".format(package_URL))
     response = urllib2.urlopen(package_URL)
@@ -63,15 +65,7 @@ def get_package_deps(package):
 
 
 if __name__ == "__main__":
-    if len(sys.argv) == 1:
-        print "At least one argument (PyPI user) is required."
-    elif len(sys.argv) == 2:
-        for package in get_package_list_by_user(sys.argv[1]):
-            for dep in get_package_deps(package):
-                print package, ": ", dep
-            print "==========================================================="
-
-    elif len(sys.argv) == 3:
+    if len(sys.argv) == 3:
         print_package_list_to_file(sys.argv[1], sys.argv[2])
     else:
         print "Expected either 1 or 2 arguments. Received {}."\

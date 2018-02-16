9bdaa75cd89d502164315f02ab45c5d229f026f1
danoscarmike <omeara.dan@gmail.com>
2018-02-15 22:49:25 -0800

    change output schema

--- dependency_sleuth.py
+++ dependency_sleuth.py
@@ -8,84 +8,99 @@ import urllib.request
 from bs4 import BeautifulSoup
 
 
-def get_packages_by_user(user):
+def main(user, destination):
+    gcp_python = {}
+    packages = get_user_packages(user)
+    for package in packages:
+        if package not in gcp_python:
+            gcp_python[package] = {}
+        versions = get_package_versions(package)
+        for version in versions:
+            if version not in gcp_python[package]:
+                gcp_python[package][version] = {}
+            dependencies = get_version_dependencies(package, version)
+            if not dependencies:
+                pass
+            else:
+                for dep in dependencies:
+                    if " " in dep:
+                        dep_name = dep.split(" ", 1)[0]
+                        dep_range = dep.split(" ", 1)[1].split(";", 1)[0]
+                    else:
+                        dep_name = dep
+                        dep_range = None
+                    if dep_name not in gcp_python[package][version]:
+                        gcp_python[package][version][dep_name] = dep_range
+    status = print_to_file(gcp_python, destination)
+    if status:
+        print("Dependencies for all versions of {}'s packages written to file."
+              .format(user))
+
+
+def get_user_packages(user):
     ''' Takes a single PyPI user (e.g. a package maintainer) and returns a list
-        of all PyPI package-versions owned by that user. '''
+        of all PyPI packages owned by that user. '''
     pypi_user_URL = "http://pypi.org/user/{}/".format(user)
-    print("Calling {}".format(pypi_user_URL))
+    print("Retrieving package list for {}. Hang tight.".format(user))
     soup = BeautifulSoup(urllib.request.urlopen(pypi_user_URL), 'html.parser')
     package_list = []
-    dependencies = {}
     for heading in soup.find_all("h3", {"class": "package-snippet__title"}):
         package_list.append(heading.get_text())
-    for package in package_list:
-        required = get_package_dependencies(package)
-        if not required:
-            pass
-        else:
-            for dist in required:
-                if " " in dist:
-                    dist_name = dist.split(" ", 1)[0]
-                    dist_range = dist.split(" ", 1)[1].split(";", 1)[0]
-                else:
-                    dist_name = dist
-                    dist_range = None
-                if dist_name not in dependencies:
-                    dependencies[dist_name] = {}
-                dependencies[dist_name][package] = dist_range
-    return package_list, dependencies
-
-
-def print_package_list_to_file(user, destination):
-    ''' Takes a single PyPI user (e.g. a package maintainer) and a local file
-        destination and writes to file all the dependencies and their versions
-        or version ranges for each PyPI package owned by the passed user. '''
-    package_list, dependencies = get_packages_by_user(user)
-    with open('%s/packages.csv' % destination, 'w') as csvfile:
-        fieldnames = ['dependency'] + package_list
-        datawriter = csv.DictWriter(csvfile, delimiter=',',
-                                    fieldnames=fieldnames, quotechar='"')
-        datawriter.writeheader()
-        for dist_name in dependencies:
-            row = {'dependency': dist_name}
-            for package_name in dependencies[dist_name]:
-                row.update({package_name:
-                            dependencies[dist_name][package_name]})
-            datawriter.writerow(row)
-    print("Packages belonging to PyPI user, {}, written to file."
-          .format(user))
-
-
-def get_package_dependencies(package):
-    ''' Takes a single PyPI package (e.g. google-cloud-speech) and returns the
-        list of required distributions for the passed package per the
-        "requires_dist" property in the "info" object of the PyPI package JSON
-        information '''
-    package_URL = "http://pypi.python.org/pypi/{}/json".format(package)
-    print("Calling {}".format(package_URL))
-    response = urllib.request.urlopen(package_URL)
-    package_json = json.load(response)
-    if 'requires_dist' not in package_json["info"]:
-        return
-    else:
-        return package_json["info"]["requires_dist"]
+    return package_list
 
 
 def get_package_versions(package):
     ''' Takes a single PyPI package (e.g. google-cloud-speech) and returns a
         list of release versions for the passed package per the
         "releases" object of the PyPI package JSON information '''
-    version_URL = "http://pypi.python.org/pypi/{}/{}/json".format(package)
-    response = urllib.request.urlopen(version_URL)
+    package_URL = "http://pypi.python.org/pypi/{}/json".format(package)
+    response = urllib.request.urlopen(package_URL)
     package_json = json.load(response)
-    versions = [package_json["info"]["version"]]
-    versions.append(release for release in packaage_json["releases"])
-    print(versions)
+    versions = []
+    for release in package_json["releases"]:
+        versions.append(release)
+    return versions
+
+
+def get_version_dependencies(package, version):
+    ''' Takes a single PyPI package and version (e.g. google-cloud-speech) and
+        returns the list of required distributions for the passed package per
+        the "requires_dist" property in the "info" object of the PyPI package
+        JSON information '''
+    version_URL = "http://pypi.python.org/pypi/{}/{}/json".format(package,
+                                                                  version)
+    response = urllib.request.urlopen(version_URL)
+    version_json = json.load(response)
+    if 'requires_dist' not in version_json["info"]:
+        return
+    else:
+        return version_json["info"]["requires_dist"]
+
+
+def print_to_file(data, destination):
+    ''' Takes a set of package dependency data and a local file destination and
+        writes to file all the dependencies and their versions or version
+        ranges for each PyPI package owned by the passed user. '''
+    fieldnames = ['package_name', 'package_version', 'dep_name', 'dep_version']
+    with open('%s/gcp_python.csv' % destination, 'w') as csvfile:
+        print("Printing to file: {}/gcp_python.csv".format(destination))
+        datawriter = csv.DictWriter(csvfile, delimiter=',',
+                                    fieldnames=fieldnames, quotechar='"')
+        datawriter.writeheader()
+        for package in data:
+            row = {'package_name': package}
+            for package_version in data[package]:
+                row.update({'package_version': package_version})
+                for dep in data[package][package_version]:
+                    row.update({'dep_name': dep, 'dep_version':
+                                data[package][package_version][dep]})
+                    datawriter.writerow(row)
+    return True
 
 
 if __name__ == "__main__":
     if len(sys.argv) == 3:
-        print_package_list_to_file(sys.argv[1], sys.argv[2])
+        main(sys.argv[1], sys.argv[2])
     else:
         print("Expected either 1 or 2 arguments. Received {}."
               .format(len(sys.argv) - 1))
